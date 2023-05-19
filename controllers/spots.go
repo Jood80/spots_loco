d607d296d0fa41b075a10264600f11e96891c08f
Jood80 <shawar.nujood@gmail.com>
2023-05-19 14:50:30 +0300

    feat: move the queries onto the code.

--- controllers/spots.go
+++ controllers/spots.go
@@ -4,18 +4,29 @@ import (
 	"database/sql"
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/lib/pq"
 )
 
 type Spot struct {
-	ID          string  `json:"id"`
-	Name        string  `json:"name"`
-	Website     sql.NullString  `json:"website"`
-	Coordinates string  `json:"coordinates"`
-	Description sql.NullString   `json:"description"`
-	Rating      float64 `json:"rating"`
+	ID          string         `json:"id"`
+	Name        string         `json:"name"`
+	Website     sql.NullString `json:"website"`
+	Coordinates string         `json:"coordinates"`
+	// Description sql.NullString `json:"description"`
+	Rating   float64 `json:"rating"`
+	Lat      float64 `json:"lat"`
+	Long     float64 `json:"long"`
+	Distance float64 `json:"distance"`
+}
+
+type Rows struct {
+	ID   string  `json:"id"`
+	Lat  float64 `json:"lat"`
+	Long float64 `json:"long"`
 }
 
 func GetSpots(w http.ResponseWriter, r *http.Request) {
@@ -29,6 +40,10 @@ func GetSpots(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	latFlo, err := strconv.ParseFloat(lat, 64)
+	longFlo, err := strconv.ParseFloat(long, 64)
+	radiusFlo, err := strconv.ParseFloat(radius, 64)
+
 	db, err := sql.Open("postgres", "postgres://postgres:123456@localhost/test?sslmode=disable")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -37,35 +52,97 @@ func GetSpots(w http.ResponseWriter, r *http.Request) {
 
 	defer db.Close()
 
-	query := fmt.Sprintf(`SELECT * FROM public."MY_TABLE" limit 10;`)
-
-	// Execute SQL statement
+	var query string
+	switch shape {
+	case "square":
+		query = fmt.Sprintf(`
+			SELECT DISTINCT id, ST_X(coordinates::geometry) AS lat, ST_Y(coordinates::geometry) AS long
+			FROM "MY_TABLE"
+			WHERE ST_X(coordinates::geometry) >= %f AND ST_X(coordinates::geometry) <= %f
+			AND ST_Y(coordinates::geometry) >= %f AND ST_Y(coordinates::geometry) <= %f`,
+			latFlo-radiusFlo, latFlo+radiusFlo, longFlo-radiusFlo, longFlo+radiusFlo)
+	case "circle":
+		query = fmt.Sprintf(`
+			SELECT DISTINCT id, ST_X(coordinates::geometry) AS lat, ST_Y(coordinates::geometry) AS long
+			FROM "MY_TABLE"
+			WHERE SQRT(ABS((%f - ST_X(coordinates::geometry)) * (%f - ST_X(coordinates::geometry)))
+			+ ABS((%f - ST_Y(coordinates::geometry)) * (%f - ST_Y(coordinates::geometry)))) <= %f`,
+			latFlo, latFlo, longFlo, longFlo, radiusFlo)
+	}
 	rows, err := db.Query(query)
 	if err != nil {
 		pqErr, ok := err.(*pq.Error)
 		if ok && pqErr.Code == "42P01" {
 			http.Error(w, "MY_TABLE table does not exist", http.StatusNotFound)
-			} else {
+		} else {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 		return
 	}
 	defer rows.Close()
 
-	// Parse results into slice of Spot objects
-	spots := []Spot{}
+	var rowData []Rows
+	var newData []Spot
+
 	for rows.Next() {
-		spot := Spot{}
-		err := rows.Scan(&spot.ID, &spot.Name, &spot.Website, &spot.Coordinates, &spot.Description, &spot.Rating)
+		var data Rows
+
+		err := rows.Scan(&data.ID, &data.Lat, &data.Long)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			log.Fatal(err)
+		}
+
+		rowData = append(rowData, data)
+	}
+
+	thirdQuery := `
+	SELECT
+		id,
+		name,
+		website,
+		coordinates,
+		rating,
+		ST_X(coordinates::geometry) AS lat,
+		ST_Y(coordinates::geometry) AS long,
+		SQRT(ABS(($1 - ST_X(coordinates::geometry)) * ($1 - ST_X(coordinates::geometry))) + ABS(($2 - ST_Y(coordinates::geometry)) * ($2 - ST_Y(coordinates::geometry)))) AS distance
+	FROM public."MY_TABLE"
+	WHERE SQRT(ABS(($1 - ST_X(coordinates::geometry)) * ($1 - ST_X(coordinates::geometry))) + ABS(($2 - ST_Y(coordinates::geometry)) * ($2 - ST_Y(coordinates::geometry)))) <= $3
+	GROUP BY id, name, website, coordinates, rating, lat, long
+	ORDER BY
+		CASE
+			WHEN SQRT(ABS(($1 - ST_X(coordinates::geometry)) * ($1 - ST_X(coordinates::geometry))) + ABS(($2 - ST_Y(coordinates::geometry)) * ($2 - ST_Y(coordinates::geometry)))) <= 50
+			THEN 0
+			ELSE SQRT(ABS(($1 - ST_X(coordinates::geometry)) * ($1 - ST_X(coordinates::geometry))) + ABS(($2 - ST_Y(coordinates::geometry)) * ($2 - ST_Y(coordinates::geometry))))
+		END ASC, rating DESC`
+
+	thirdRows, err := db.Query(thirdQuery, latFlo, longFlo, radiusFlo*radiusFlo)
+	if err != nil {
+		println("Error querying")
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		log.Fatal(err)
+	}
+	defer thirdRows.Close()
+
+	for thirdRows.Next() {
+		println("we're here")
+		var data Spot
+
+		err := thirdRows.Scan(&data.ID, &data.Name, &data.Website, &data.Coordinates, &data.Rating, &data.Lat, &data.Long, &data.Distance)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
-		spots = append(spots, spot)
+		newData = append(newData, data)
+		println("rowData", rowData)
 	}
 
-	// Convert slice of Spot objects to JSON and return it
-	jsonData, err := json.Marshal(spots)
+	if err = rows.Err(); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		log.Fatal(err)
+	}
+
+	jsonData, err := json.Marshal(newData)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
--- get_spots.session.sql
+++ get_spots.session.sql
@@ -12,16 +12,19 @@ WHERE SQRT(ABS((1- ST_X(coordinates::geometry))* (1-ST_X(coordinates::geometry))
 
 SELECT
 	id,
+	name,
+	website,
+	coordinates,
 	rating,
 	ST_X(coordinates::geometry) AS lat,
 	ST_Y(coordinates::geometry) AS long,
 	SQRT(ABS((1- ST_X(coordinates::geometry))* (1-ST_X(coordinates::geometry))) + ABS((2-ST_Y(coordinates::geometry))*(2-ST_Y(coordinates::geometry)))) AS distance
 FROM "MY_TABLE"
 WHERE SQRT(ABS((1- ST_X(coordinates::geometry))* (1-ST_X(coordinates::geometry))) + ABS((2-ST_Y(coordinates::geometry))*(2-ST_Y(coordinates::geometry)))) <= 100
-GROUP BY id, rating, lat, long
+GROUP BY id, rating, lat, long, name, website, coordinates
 ORDER BY  
 Case 
-WHEN SQRT(ABS((1- ST_X(coordinates::geometry))* (1-ST_X(coordinates::geometry))) + ABS((2-ST_Y(coordinates::geometry))*(2-ST_Y(coordinates::geometry))))  <=50
+WHEN SQRT(ABS((1- ST_X(coordinates::geometry))* (1-ST_X(coordinates::geometry))) + ABS((2-ST_Y(coordinates::geometry))*(2-ST_Y(coordinates::geometry))))  <= 50
 Then 0
 ELSE SQRT(ABS((1- ST_X(coordinates::geometry))* (1-ST_X(coordinates::geometry))) + ABS((2-ST_Y(coordinates::geometry))*(2-ST_Y(coordinates::geometry)))) 
 END ASC, rating DESC;
